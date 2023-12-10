113d7f26cc45d07abbf43517170e12b7c832b493
Shahbaaz <59239856+Shahbaaz92@users.noreply.github.com>
2023-12-10 20:13:47 +0530

    updated everything

--- script.js
+++ script.js
@@ -2,15 +2,22 @@ const slideShow = document.getElementById("slideshow");
 const brandLogo = document.getElementById("brand-logo");
 const gallery = document.getElementById("gallery");
 const paintingSlide = document.getElementById("painting-slide");
+const nextSlide = document.getElementById("next-slide");
+const prevSlide = document.getElementById("prev-slide");
+const paintingSlideSub = document.getElementById("painting-currentSlide");
+const paintings = document.querySelectorAll(".painting");
 
 brandLogo.addEventListener("click", function () {
   gallery.classList.remove("hidden");
   paintingSlide.classList.add("hidden");
   slideShow.innerText = `Start Slideshow`;
+  currentIndex = 0;
 });
 
 slideShow.addEventListener("click", function () {
   if (slideShow.innerText.includes("START")) {
+    nextSlide.removeAttribute("disabled");
+    prevSlide.removeAttribute("disabled");
     slideShow.innerText = `Stop Slideshow`;
     gallery.classList.add("hidden");
     paintingSlide.classList.remove("hidden");
@@ -18,7 +25,20 @@ slideShow.addEventListener("click", function () {
     getData();
   } else {
     slideShow.innerText = `Start Slideshow`;
+    nextSlide.setAttribute("disabled", true);
+    prevSlide.setAttribute("disabled", true);
   }
+});
+
+let currentIndex = 0;
+
+nextSlide.addEventListener("click", function () {
+  currentIndex++;
+  getData();
+});
+prevSlide.addEventListener("click", function () {
+  currentIndex--;
+  getData();
 });
 
 async function getData() {
@@ -26,27 +46,28 @@ async function getData() {
   let data = await response.json();
   console.log(data);
 
-  let currentIndex = 0;
-
   for (let i = 0; i < data.length; i++) {
     let currentSLide = data[currentIndex];
-    console.log(currentSLide.name);
-
-    paintingSlide.innerHTML = `
+    paintingSlideSub.innerHTML = `
     <section class="painting-info">
       <article class="the-artbox">
         <img
           src=${currentSLide.images.hero.large}
           alt="Starry night by Vincent"
           value =${currentSLide.name}
+          class='artbox-image'
         />
-        <button class="modal-button">
+        <button data-open-modal class="modal-button"  id='modal-button'>
           <img
             src="./assets/shared/icon-view-image.svg"
             alt="view image"
           />
           VIEW IMAGE
         </button>
+        <dialog data-modal class='modal'> 
+         <a class='close-modal' data-close-modal>Close</a>
+          <img src=${currentSLide.images.hero.large}>
+          </dialog>   
       </article>
       <article class="the-artpainter">
         <div class="the-artName">
@@ -67,7 +88,7 @@ async function getData() {
             ${currentSLide.description}
           </p>
         </div>
-        <a href=${currentSLide.source} class="art-source">
+        <a href=${currentSLide.source} class="art-source" target=_blank>
           go to source
         </a>
       </article>
@@ -80,15 +101,30 @@ async function getData() {
         <h3>${currentSLide.name}</h3>
         <small>${currentSLide.artist.name}</small>
       </article>
-      <article class="buttons-slide">
-        <button class="prev-slide btn" id='prev-slide'>
-          <img src="./assets/shared/icon-back-button.svg" alt="previous slide" />
-        </button>
-        <button class="next-slide btn" id='next-slide'>
-          <img src="./assets/shared/icon-next-button.svg" alt="next slide" />
-        </button>
-      </article>
-    </div>;
+    </div>
   `;
+
+    const modalOpenBtn = document.querySelector("[data-open-modal]");
+    const modal = document.querySelector("[data-modal]");
+    const modalcloseBtn = document.querySelector("[data-close-modal]");
+    modalOpenBtn.addEventListener("click", function () {
+      modal.showModal();
+    });
+    modalcloseBtn.addEventListener("click", function () {
+      modal.close();
+    });
   }
+}
+
+for (let i = 0; i < paintings.length; i++) {
+  paintings[i].addEventListener("click", function () {
+    currentIndex = i;
+
+    nextSlide.removeAttribute("disabled");
+    prevSlide.removeAttribute("disabled");
+    slideShow.innerText = `Stop Slideshow`;
+    gallery.classList.add("hidden");
+    paintingSlide.classList.remove("hidden");
+    getData();
+  });
 }
